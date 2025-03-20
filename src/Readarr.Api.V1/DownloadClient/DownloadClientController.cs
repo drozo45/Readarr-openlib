080dd301f33d295b0b1fd62a2d7c4cfd648955b6
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-03-20 13:10:06 +0200

    Fixed: Priority validation for indexers and download clients
    (cherry picked from commit f0e320f3aa501f120721503b8256f464a31be783)

--- src/Readarr.Api.V1/DownloadClient/DownloadClientController.cs
+++ src/Readarr.Api.V1/DownloadClient/DownloadClientController.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using NzbDrone.Core.Download;
 using Readarr.Http;
 
@@ -12,6 +13,7 @@ namespace Readarr.Api.V1.DownloadClient
         public DownloadClientController(IDownloadClientFactory downloadClientFactory)
             : base(downloadClientFactory, "downloadclient", ResourceMapper, BulkResourceMapper)
         {
+            SharedValidator.RuleFor(c => c.Priority).InclusiveBetween(1, 50);
         }
     }
 }
